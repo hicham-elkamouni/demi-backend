8dfd2c64ab3272dea3a7bcde29b025641641e101
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-19 21:32:45 +0100

    updating on post service

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Post } from './post.schema';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectModel(Post.name) private readonly postModel: Model<Post>,
+  ) {}
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  //   update(id: number, updateDonatorDto: UpdateDonatorDto) {
+  //     return `This action updates a #${id} user`;
+  //   }
+
+  //   remove(id: number) {
+  //     return `This action removes a #${id} user`;
+  //   }
+}
