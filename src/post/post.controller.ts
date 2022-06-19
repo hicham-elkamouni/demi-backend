55137e15fd6f2fbb4ada93a77a4ef012b8f44e3f
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-19 21:55:27 +0100

    updating on post controller and post service

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { Role } from 'src/decorators/role.decorator';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  async create(@Body() createPostDto: CreatePostDto) {
+    console.log('inside create post');
+    try {
+      return this.postService.create(createPostDto);
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreatePostDto } from './dto/create-post.dto';
 import { Post } from './post.schema';
 
 @Injectable()
@@ -8,6 +9,16 @@ export class PostService {
   constructor(
     @InjectModel(Post.name) private readonly postModel: Model<Post>,
   ) {}
+
+  async create(createPostDto: CreatePostDto) {
+    try {
+      const post = new this.postModel(createPostDto);
+      let NewPost = await post.save();
+      return NewPost;
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
 
   findAll() {
     return `This action returns all user`;
