99c6610cada1d3f040d21f254db7eeedfd9f37d3
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-19 21:20:09 +0100

    adding post schema

---
+++ src/post/post.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Document } from 'mongoose';
+import { Donator } from 'src/donator/donator.schema';
+
+@Schema({ timestamps: true })
+export class Post extends Document {
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  bloodType: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Donator' })
+  createdBy: Donator;
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
