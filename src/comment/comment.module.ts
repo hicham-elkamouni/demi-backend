b8aa8309e8799323908c26c2ca5e244bbad62ff2
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-22 09:26:50 +0100

    updating on comment module

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
+import { CommentController } from './comment.controller';
+import { CommentSchema } from './comment.schema';
 import { CommentService } from './comment.service';
 
 @Module({
-  providers: [CommentService]
+  imports: [
+    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
+    AuthModule,
+  ],
+  controllers: [CommentController],
+  providers: [CommentService],
 })
 export class CommentModule {}
