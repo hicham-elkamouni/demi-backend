e37fff104c0db0c83f21686c4e600234ac990b8f
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-28 09:33:21 +0100

    add small comment

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,9 +1,8 @@
 import {
-
-    CanActivate,
-    ExecutionContext,
-    Injectable,
-    UnauthorizedException
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
@@ -11,20 +10,20 @@ import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
-    constructor(private reflector: Reflector, private authService: AuthService) { }
+  constructor(private reflector: Reflector, private authService: AuthService) {}
 
-    async canActivate(context: ExecutionContext): Promise<any> {
-        const request = context.switchToHttp().getRequest();
-        const role = this.reflector.get<string>('role', context.getHandler());
-        const Authorization = request.headers.authorization;//
-        if (!Authorization) throw new UnauthorizedException();
+  // get token from header
+  async canActivate(context: ExecutionContext): Promise<any> {
+    const request = context.switchToHttp().getRequest();
+    const role = this.reflector.get<string>('role', context.getHandler());
+    const Authorization = request.headers.authorization; //
+    if (!Authorization) throw new UnauthorizedException();
 
-        const token = Authorization.split(' ')[1];
-        const user = await this.authService.verifyToken(token, role);
-        if (!user) throw new UnauthorizedException();
-        request.user = user;//
+    const token = Authorization.split(' ')[1];
+    const user = await this.authService.verifyToken(token, role);
+    if (!user) throw new UnauthorizedException();
+    request.user = user; //
 
-        return true;
-    }
-
+    return true;
+  }
 }
